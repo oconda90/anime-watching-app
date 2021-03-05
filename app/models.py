401582f205443ec95bfb43fd4768eabe1f960f02
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-03-05 00:17:09 -0800

    2 more models

--- app/models.py
+++ app/models.py
@@ -32,3 +32,33 @@ class Anime(db.Model):
 
     def __str__(self):
         return f'Anime: {self.title}'
+    
+class Studio(db.Model):
+    """Studio model."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    about = db.Column(db.String(350))
+    
+    # The Anime animated by the Studio
+    animes = db.relationship('Anime', back_populates='Studio')
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+class Genre(db.Model):
+    """Genre model."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+
+    # The genre a anime belongs to
+    songs = db.relationship(
+        'Anime', secondary='anime_genre', back_populates='genres')
+
+    def __str__(self):
+        return f'{self.name}'
+
+anime_genre_table = db.Table('anime_genre',
+    db.Column('anime_id', db.Integer, db.ForeignKey('anime.id')),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+)
