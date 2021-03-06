909f182a08b83fb0dafd8041fff84ffda270ecff
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-03-05 22:42:51 -0800

    added forms to auth

---
+++ app/auth/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+from app.models import User
+from app import bcrypt
+
+#Forms: 
+# SignUpForm, LoginForm
+
+
+class SignUpForm(FlaskForm):
+    """Sign up Form"""
+    username = StringField('Username',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    name = StringField('Full Name',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if not user:
+            raise ValidationError('No user with that username. Please try again.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user and not bcrypt.check_password_hash(
+                user.password, password.data):
+            raise ValidationError('Password doesn\'t match. Please try again.')
