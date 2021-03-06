bf5656e565797caac9bfc663d52355889e505110
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-03-05 21:47:34 -0800

    2 more routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -18,7 +18,7 @@ main = Blueprint("main", __name__)
 @main.route('/')
 def home():
     '''Display homepage with all playlists from users'''
-    all_watchlists = watchlist.query.all()
+    all_watchlists = Watchlist.query.all()
     return render_template('home.html',
         all_watchlists=all_watchlists)
 
@@ -31,3 +31,34 @@ def profile(username):
     current_watchlists = user.watchlists
     return render_template('profile.html',
         current_watchlists=current_watchlists)
+    
+@main.route('/anime_in_watchlist/<id>')
+@login_required
+def anime_in_watchlist(id):
+    '''Display selected watchlist with all animes from that watchlist'''
+    selected_watchlist = Watchlist.query.filter_by(id=id).one()
+    all_animes = selected_Watchlist.songs
+    return render_template('anime_in_watchlist.html',
+    all_animes=all_animes)
+
+
+@main.route('/create_anime', methods=['GET', 'POST'])
+@login_required
+def create_anime():
+    '''Create a Anime Route'''
+    form = AnimeForm()
+    if form.validate_on_submit(): 
+        new_anime = Anime(
+            title=form.title.data,
+            photo_url = form.photo_url.data,
+            date=form.date.data,
+            studio=form.studio.data,
+            genres=form.genres.data,
+            watchlists=form.watchlists.data
+        )
+        db.session.add(new_anime)
+        db.session.commit()
+
+        flash('New anime was created successfully.')
+        return redirect(url_for('main.anime_detail', anime_id=new_anime.id))
+    return render_template('create_anime.html', form=form)
